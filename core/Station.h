65d44eb87e51944d33e888722445f6a15fc1948c
Mason Speck <mcs.5299@gmail.com>
2024-11-03 20:36:14 -0500

    Station wip

--- core/Station.h
+++ core/Station.h
@@ -1,1 +1,23 @@
-#pragma once
+#ifndef SIMULA_CORE_STATION_H
+#define SIMULA_CORE_STATION_H
+
+#include "core/ClassRegistry.h"
+#include "core/Component.h"
+
+namespace simula {
+namespace core {
+
+class Station : public Component {
+public:
+    Station();
+    ~Station();
+
+public:
+    static constexpr ClassData classData() {
+        return { "Station" };
+    }
+};
+
+} // end core
+} // end simula
+#endif
